e522d19b03bd2624b55abe5c62e91d7d49ed2800
Mezzky <ryansahadha@gmail.com>
2022-05-05 21:47:31 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Navbar Toggle
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('header ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
